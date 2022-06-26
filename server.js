4b3669bb5e29453318825f7647659c5b9e843ab8
peayah <82358003+peayah@users.noreply.github.com>
2022-06-25 20:51:09 -0700

    add two entries in api

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const heroes = {
+    "captain america": {
+        "city": "Washington DC",
+        "moviesAppear" : ["Captain America: The First Avenger", "Captain America: The Winter Soldier", "Captain America: Civil War"],
+        'moviesMention' : []
+    ,}
+    "Ironman": {
+        "city": "New York",
+        "moviesAppear" : ["Ironman", "Ironman 2", 'Ironman 3'],
+        'moviesMention' : []
+
+    }
+}
+
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
