5d279578cc6b2943db7f708f2d8d4098679edf0c
peayah <82358003+peayah@users.noreply.github.com>
2022-06-25 21:22:34 -0700

    save json to own file

--- server.js
+++ server.js
@@ -2,26 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-const heroes = {
-    "captain america": {
-        "city": "Washington DC",
-        "moviesAppear" : ["Captain America: The First Avenger", "Captain America: The Winter Soldier", "Captain America: Civil War"],
-        'moviesMention' : []
-    },
-    "ironman": {
-        "city": "New York",
-        "moviesAppear" : ["Ironman", "Ironman 2", 'Ironman 3'],
-        'moviesMention' : []
-
-    },
-     "Scooby Doo": {
-        "city": "Scooby Doo",
-        "moviesAppear" : ["Scooby Doo"],
-        'moviesMention' : ["Scooby Doo"]
-
-    }
-}
-
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
