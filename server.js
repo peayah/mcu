fc8da8f0b31cb6cce1a69e0ccc5cddd0d089f528
peayah <82358003+peayah@users.noreply.github.com>
2022-06-25 23:29:25 -0700

    add env PORT

--- server.js
+++ server.js
@@ -2,21 +2,41 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const heroes = {
+    "captain america": {
+        "city": "Washington DC",
+        "moviesAppear" : ["Captain America: The First Avenger", "Captain America: The Winter Soldier", "Captain America: Civil War"],
+        'moviesMention' : []
+    },
+    "ironman": {
+        "city": "New York",
+        "moviesAppear" : ["Ironman", "Ironman 2", 'Ironman 3'],
+        'moviesMention' : []
+    },
+    "Scooby Doo": {
+        "city": "Scooby Doo",
+        "moviesAppear" : ["Scooby Doo"],
+        'moviesMention' : ["Scooby Doo"]
+    },
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
 app.get('/api/:heroName', (request, response) => {
-    const heroName  = (request.params.heroName).toLowerCase()
+
+    const herosName  = request.params.heroName.toLowerCase()
     
-    if (heroes[heroName]) {
-        response.json(heroes[heroName])       
+    if (heroes[herosName]) {
+        response.json(heroes[herosName])       
     } else {
         response.json(heroes['Scooby Doo'])
     }
     
+
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`"Server is running on ${PORT}"`)
 })
