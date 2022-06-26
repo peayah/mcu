6df4624f04a46690644992feae98b26753d9141a
peayah <82358003+peayah@users.noreply.github.com>
2022-06-26 16:51:59 -0700

    add samle result and return result

--- js/main.js
+++ js/main.js
@@ -9,10 +9,31 @@ function getFetch() {
         .then(res => res.json()) // parse response as JSON
         .then(data => {
         console.log(url)
-       
+
         document.getElementById("city-name").innerText = data.city
-        })
+        
+        l   et appearanceUl = document.getElementById("movie-appearance")
+        for (var i = 0; i < data.moviesAppear.length; i++) {
+            var appearance = data.moviesAppear[i];
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(appearance));
+            appearanceUl.appendChild(li);
+        } // end for loop
+        let mentionUl = document.getElementById("movie-mention")
+        if(data.moviesMention.length == 0) {
+            var p = document.createElement('li');
+            p.appendChild(document.createTextNode("No mentions at this time"));
+            mentionUl.appendChild(p);
+        } else { 
+            for (var i = 0; i < data.moviesMention.length; i++) {
+                var mention = data.moviesMention[i];
+                var li = document.createElement('li');
+                li.appendChild(document.createTextNode(mention));
+                mentionUl.appendChild(li);
+            }
+        } // end else
+})
         .catch(err => {
         console.log(`error ${err}`)
         });
-}
+} // endget fetch
